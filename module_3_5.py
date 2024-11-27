e0f1883005f4e9c9fcfc7ae037bf865b70121788
Nick Fedorenko <nfm12121994@gmail.com>
2024-11-27 04:06:49 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,16 @@
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+
+    if len(str_number) == 1:
+        return first
+
+    else:
+        if len(str_number) > 1 and str_number[-1] == '0':
+            str_number = str_number[:-1]
+        if len(str_number) == 1:
+            return first
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+print(get_multiplied_digits(3303))
